b461c95a90219b7619dec9afde9224114c03bb68
Pedro Gorobey <up202210292@up.pt>
2025-12-04 01:00:56 +0000

    Improved collision handling by 'sliding' the submarine rather than stopping it

--- pw2/objects/submarine/SubmarineControls.js
+++ pw2/objects/submarine/SubmarineControls.js
@@ -121,12 +121,38 @@ class SubmarineControls {
         this.submarine.position.add(dv);
         boundingSphere.center.add(this.submarine.position);
 
+        const trianglesIntersected = [];
         for (const collider of this.colliders) {
-            if (collider.intersectsSphere(boundingSphere)) {
+            collider.shapecast({
+                intersectsBounds: (box) => box.intersectsSphere(boundingSphere),
+                intersectsTriangle: (tri) => {
+                    if (tri.intersectsSphere(boundingSphere)) {
+                        trianglesIntersected.push(tri);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            if (trianglesIntersected.length > 0) {
                 this.submarine.position.sub(dv);
-                this.submarine.forwardSpeed = 0;
-                this.submarine.verticalSpeed = 0;
-                return;
+
+                const normal = new THREE.Vector3();
+                trianglesIntersected[0].getNormal(normal); // TODO: is it possible/common to get more than 1 triangle?
+
+                // vslide​=v−(v⋅n)n
+                const slide = dv.clone().sub(normal.clone().multiplyScalar(dv.clone().dot(normal)));
+                this.submarine.position.add(slide);
+                boundingSphere.center.sub(dv).add(slide);
+
+                for (const collider of this.colliders) {
+                    if (collider.intersectsSphere(boundingSphere)) {
+                        this.submarine.position.sub(slide);
+                        // this.submarine.forwardSpeed = 0;
+                        // this.submarine.verticalSpeed = 0;
+                        return;
+                    }
+                }
             }
         }
 
