8749bf0d5db5d2edf314bdfd00f351a38ced30ca
Pedro Gorobey <up202210292@up.pt>
2025-12-16 10:30:15 +0000

    Code cleanup: added an onLoad method to terrain displacement loading

--- pw2/MyContents.js
+++ pw2/MyContents.js
@@ -43,6 +43,7 @@ class MyContents  {
         this.seafloorGroup = null;
         this.terrainSize = 100;
         this.terrain = new MyTerrain(this, this.terrainSize);
+        this.affectedByTerrain = []; // objects whose position depends on the terrain
         this.rocks = null;
         this.coralMeshes = null;
 
@@ -115,6 +116,8 @@ class MyContents  {
             }
         });
 
+        this.affectedByTerrain.push(...this.groupHorsePillars.children);
+
         // this.horse3 = new HorsePillar(this.app);
         // this.horse3.position.set(-25, 0, -64);
         // this.horse3.rotateY(-Math.PI/2);
@@ -152,6 +155,7 @@ class MyContents  {
         this.vase.position.set(10, 0, 10);
         this.app.scene.add(this.vase);
         this._vasePositioned = false;
+        this.affectedByTerrain.push(this.vase);
 
         this.chest = this.fastLoad
             ? new THREE.Mesh(new THREE.BoxGeometry())
@@ -159,6 +163,7 @@ class MyContents  {
         this.chest.position.set(1, 0, 18);
         this.chest.rotateY(-Math.PI/4);
         this.app.scene.add(this.chest);
+        this.affectedByTerrain.push(this.chest);
 
 
         
@@ -240,6 +245,7 @@ class MyContents  {
                     break;
                 }
             }
+            this.affectedByTerrain.push(coral);
             this.corals.push(coral);
         }
         this.coralMeshes.add(TubeCoral.defaultOwner);
@@ -523,6 +529,9 @@ class MyContents  {
 
         this.setupClickHandler();
 
+        // ALWAYS leave this near near the end to ensure all necessary objects are already on the scene
+        this.terrain.loadDisplacement(this.afterTerrainLoads.bind(this));
+
         // // CLIPPING ///////////
         // // clipping plane at y = 0
         // const clippingPlanes = [
@@ -563,37 +572,9 @@ class MyContents  {
         const dt = this._lastUpdateTime ? Math.min(0.1, now - this._lastUpdateTime) : 0;
         this._lastUpdateTime = now;
 
-        if (!this._horsePositioned && this.terrain?.displacementMap) {
-            
-            this.groupHorsePillars.children.forEach((horse) => {
-            const terrainHeight = this.terrain.displacementAtXY(horse.position.x, horse.position.z);
-            horse.position.y = terrainHeight;
-
-            // Apply terrain inclination to rotate horse with the terrain
-            const inclination = this.terrain.inclinationAtXY(horse.position.x, horse.position.z);
-                horse.rotateX(inclination[1]);
-                horse.rotateZ(-inclination[0]);
-            });
-            
-            this._horsePositioned = true;
-        }
-
         if (!this._clippingApplied && this.apollo.isLoaded && this.apollo.isLoaded()) {
             this.applyClipping();
             this._clippingApplied = true;
-        }
-
-        if (!this._vasePositioned && this.terrain?.displacementMap) {
-            
-            const terrainHeight = this.terrain.displacementAtXY(this.vase.position.x, this.vase.position.z);
-            this.vase.position.y = terrainHeight;
-
-            // Apply terrain inclination to rotate vase with the terrain
-            const inclination = this.terrain.inclinationAtXY(this.vase.position.x, this.vase.position.z);
-            this.vase.rotateX(inclination[1]);
-            this.vase.rotateZ(-inclination[0]);
-            
-            this._vasePositioned = true;
         }
                                                                             
         if (this.sharkController) {
@@ -774,6 +755,79 @@ class MyContents  {
         }
     });
 }
+
+    afterTerrainLoads() {
+        this.colliders.push(SgiUtils.buildColliderGeo(this.terrain).boundsTree);
+        const seafloorGroup = this.app.scene.getObjectByName("seafloorGroup");
+        this.affectedByTerrain.forEach((coral) => {
+            const x = coral.position.x;
+            const y = coral.position.z;
+
+            coral.position.y += this.terrain.displacementAtXY(x, y);
+            const rotation = this.terrain.inclinationAtXY(x, y);
+            coral.rotateX(rotation[1]);
+            coral.rotateZ(-rotation[0]);
+        });
+        TubeCoral.defaultOwner.updateInstances(() => {});
+        BrainCoral.defaultOwner.updateInstances(() => {});
+        LSystemCoral.defaultOwner.updateInstances(() => {});
+        seafloorGroup.getObjectByName("rocks").children.forEach((rock) => rock.position.y += this.terrain.displacementAtXY(rock.position.x, rock.position.z));
+
+        // calculate BVH only after terrain's displacement
+        let xx1 = +Infinity, xx2 = -Infinity, yy1 = +Infinity, yy2 = -Infinity, zz1 = +Infinity, zz2 = -Infinity;
+        this.corals.forEach(coral => {
+            let x1 = +Infinity, x2 = -Infinity, y1 = +Infinity, y2 = -Infinity, z1 = +Infinity, z2 = -Infinity;
+            coral._instances.forEach(obj => {
+                const box = obj.owner.bvh.nodesMap.get(obj.id).box;
+                x1 = Math.min(x1, box[0]);
+                x2 = Math.max(x2, box[1]);
+                y1 = Math.min(y1, box[2]);
+                y2 = Math.max(y2, box[3]);
+                z1 = Math.min(z1, box[4]);
+                z2 = Math.max(z2, box[5]);
+            });
+            xx1 = Math.min(xx1, x1);
+            xx2 = Math.max(xx2, x2);
+            yy1 = Math.min(yy1, y1);
+            yy2 = Math.max(yy2, y2);
+            zz1 = Math.min(zz1, z1);
+            zz2 = Math.max(zz2, z2);
+
+            coral.box = new THREE.Box3(
+                new THREE.Vector3(x1, y1, z1),
+                new THREE.Vector3(x2, y2, z2),
+            );
+
+            this.coralsBVHHelper.add(new THREE.Box3Helper(coral.box));
+        });
+
+        const grid = [];
+        const gridSize = 6;
+        const dx = (xx2 - xx1) / gridSize, dz = (zz2 - zz1) / gridSize;
+        for (let i = 0; i < gridSize; ++i) {
+            for (let j = 0; j < gridSize; ++j) {
+                const child = {
+                    box: new THREE.Box3(
+                        new THREE.Vector3(xx1 + i * dx, yy1, zz1 + j * dz),
+                        new THREE.Vector3(xx1 + (i+1) * dx, yy2, zz1 + (j+1) * dz),
+                    ),
+                };
+                child.children = this.corals
+                    .filter(coral => coral.box.intersectsBox(child.box))
+                    .map(coral => {return {box: coral.box, obj: coral}})
+
+                grid.push(child);
+                this.coralsBVHHelper.add(new THREE.Box3Helper(child.box, 0x00ff00));
+            }
+        }
+
+        this.coralsBVH.box = new THREE.Box3(
+            new THREE.Vector3(xx1, yy1, zz1),
+            new THREE.Vector3(xx2, yy2, zz2),
+        );
+        this.coralsBVH.children = grid;
+        this.app.scene.add(this.coralsBVHHelper);
+    }
 }
 
 export { MyContents };
--- pw2/objects/terrain/MyTerrain.js
+++ pw2/objects/terrain/MyTerrain.js
@@ -1,8 +1,4 @@
 import * as THREE from 'three';
-import { TubeCoral } from '../corals/TubeCoral.js';
-import { BrainCoral } from '../corals/BrainCoral.js';
-import { LSystemCoral } from '../corals/LSystemCoral.js';
-import { SgiUtils } from '../../SgiUtils.js';
 
 class MyTerrain extends THREE.Object3D {
     #width;
@@ -10,102 +6,35 @@ class MyTerrain extends THREE.Object3D {
     #canvas;
     #canvasCtx;
 
-    constructor(contents, size = 100) {
+    constructor(contents, size = 100, autoLoadDisplacement = false, onLoad = () => {}) {
         super();
-        this.contents = contents;
-        this.app = contents.app;
         this.#width = size;
         this.#length = size;
-        this.displacementMap = null;
-        this.displacementScale = 5;
+        if (autoLoadDisplacement)
+            this.loadDisplacement(onLoad);
+        this.displacementScale = 5; // displacement calculated manually to allow coiso e tal
         this.buildTerrain();
+    }
+
+    loadDisplacement(onLoad = () => {}) {
+        if (!this.displacementMap) {
+            const terrainMap = new THREE.TextureLoader().load('images/heightmap.jpg', () => {
+                this.displacementMap = terrainMap.image;
+                const geo = this.children[0].geometry;
+                const posArr = geo.attributes.position.array;
+                for (let i = 0; i < posArr.length; i+=3) {
+                    posArr[i+2] += this.displacementAtXY(posArr[i], -posArr[i+1]);
+                }
+                geo.attributes.position.needsUpdate = true;
+                onLoad();
+            });
+            terrainMap.wrapS = terrainMap.wrapT = THREE.RepeatWrapping;
+            terrainMap.repeat.set(1, 1);
+        }
     }
 
     buildTerrain() {
         const terrainGeometry = new THREE.PlaneGeometry(this.#width, this.#length, 64, 64);
-
-        let terrainMap = new THREE.TextureLoader().load('images/heightmap.jpg', () => {
-            this.displacementMap = terrainMap.image;
-            const geo = this.children[0].geometry;
-            const posArr = geo.attributes.position.array;
-            for (let i = 0; i < posArr.length; i+=3) {
-                posArr[i+2] += this.displacementAtXY(posArr[i], -posArr[i+1]);
-            }
-            geo.attributes.position.needsUpdate = true;
-            this.contents.colliders.push(SgiUtils.buildColliderGeo(this).boundsTree);
-            const seafloorGroup = this.app.scene.getObjectByName("seafloorGroup");
-            this.contents.corals.forEach((coral) => {
-                const x = coral.position.x;
-                const y = coral.position.z;
-
-                coral.position.y += this.displacementAtXY(x, y);
-                const rotation = this.inclinationAtXY(x, y);
-                coral.rotateX(rotation[1]);
-                coral.rotateZ(-rotation[0]);
-            });
-            TubeCoral.defaultOwner.updateInstances(() => {});
-            BrainCoral.defaultOwner.updateInstances(() => {});
-            LSystemCoral.defaultOwner.updateInstances(() => {});
-            seafloorGroup.getObjectByName("rocks").children.forEach((rock) => rock.position.y += this.displacementAtXY(rock.position.x, rock.position.z));
-            this.#canvas = this.#canvasCtx = undefined;
-
-            let xx1 = +Infinity, xx2 = -Infinity, yy1 = +Infinity, yy2 = -Infinity, zz1 = +Infinity, zz2 = -Infinity;
-            this.contents.corals.forEach(coral => {
-                let x1 = +Infinity, x2 = -Infinity, y1 = +Infinity, y2 = -Infinity, z1 = +Infinity, z2 = -Infinity;
-                coral._instances.forEach(obj => {
-                    const box = obj.owner.bvh.nodesMap.get(obj.id).box;
-                    x1 = Math.min(x1, box[0]);
-                    x2 = Math.max(x2, box[1]);
-                    y1 = Math.min(y1, box[2]);
-                    y2 = Math.max(y2, box[3]);
-                    z1 = Math.min(z1, box[4]);
-                    z2 = Math.max(z2, box[5]);
-                });
-                xx1 = Math.min(xx1, x1);
-                xx2 = Math.max(xx2, x2);
-                yy1 = Math.min(yy1, y1);
-                yy2 = Math.max(yy2, y2);
-                zz1 = Math.min(zz1, z1);
-                zz2 = Math.max(zz2, z2);
-
-                coral.box = new THREE.Box3(
-                    new THREE.Vector3(x1, y1, z1),
-                    new THREE.Vector3(x2, y2, z2),
-                );
-
-                this.contents.coralsBVHHelper.add(new THREE.Box3Helper(coral.box));
-            });
-
-            const grid = [];
-            const gridSize = 6;
-            const dx = (xx2 - xx1) / gridSize, dz = (zz2 - zz1) / gridSize;
-            for (let i = 0; i < gridSize; ++i) {
-                for (let j = 0; j < gridSize; ++j) {
-                    const child = {
-                        box: new THREE.Box3(
-                            new THREE.Vector3(xx1 + i * dx, yy1, zz1 + j * dz),
-                            new THREE.Vector3(xx1 + (i+1) * dx, yy2, zz1 + (j+1) * dz),
-                        ),
-                    };
-                    child.children = this.contents.corals
-                        .filter(coral => coral.box.intersectsBox(child.box))
-                        .map(coral => {return {box: coral.box, obj: coral}})
-
-                    grid.push(child);
-                    this.contents.coralsBVHHelper.add(new THREE.Box3Helper(child.box, 0x00ff00));
-                }
-            }
-
-            this.contents.coralsBVH.box = new THREE.Box3(
-                new THREE.Vector3(xx1, yy1, zz1),
-                new THREE.Vector3(xx2, yy2, zz2),
-            );
-            this.contents.coralsBVH.children = grid;
-            this.app.scene.add(this.contents.coralsBVHHelper);
-        });
-
-        terrainMap.wrapS = terrainMap.wrapT = THREE.RepeatWrapping;
-        terrainMap.repeat.set(1, 1);
 
         const texture = new THREE.TextureLoader().load('textures/sand.jpg');
         texture.wrapT = texture.wrapS = THREE.RepeatWrapping;
@@ -113,9 +42,7 @@ class MyTerrain extends THREE.Object3D {
 
         const terrainMaterial = new THREE.MeshPhongMaterial({ 
             color: "#8B7355",
-            // displacementMap: terrainMap,
-            // displacementScale: 5,
-            map: texture
+            map: texture,
         });
 
         const terrainMesh = new THREE.Mesh(terrainGeometry, terrainMaterial);
@@ -159,8 +86,8 @@ class MyTerrain extends THREE.Object3D {
         x = u * image.width;
         y = (1 - v) * image.height;
         const pixel = ctx.getImageData(x, y, 1, 1).data;
-        // displacement will depend on brightest color (usually doesn't matter because typically these maps are grayscaled)
-        return this.displacementScale * Math.max(...pixel.slice(0, 3)) / 255;
+        // assuming map is grayscaled
+        return this.displacementScale * Math.max(pixel[0]) / 255;
     }
 
     toggleWireframe(wireframe) {
